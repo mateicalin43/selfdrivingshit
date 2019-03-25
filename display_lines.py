f20f9520f882e89ded711518bde2e7a1d0dd9846
mateicalin43 <47333829+mateicalin43@users.noreply.github.com>
2019-03-25 20:36:04 +0200

    Create display_lines.py

---
+++ display_lines.py
@@ -0,0 +1,10 @@
+# display lines
+
+
+def display_lines(image, lines):
+    line_image = np.zeros_like(image)
+    if lines is not None:
+        for line in lines:
+            x1, y1, x2, y2 = line.reshape(4)
+            cv2.line(line_image, (x1, y1), (x2, y2), (255, 0, 0), 10)
+    return line_image
